eeea2b6a5fdccbc8d72b693f72841729ef2d57ef
CheenoOng09 <95478650+CheenoOng09@users.noreply.github.com>
2025-07-21 13:11:49 +0800

    Add files via upload

---
+++ Week 10 Polymorphism for Shapes Part 1/Shape.h
@@ -0,0 +1,89 @@
+#ifndef SHAPE_H
+#define SHAPE_H
+
+#include <iostream>
+using namespace std;
+
+class Pt
+{
+private:
+    double x;
+    double y;
+public:
+    Pt(const double &x1 = 0, const double &y1 = 0)
+        :x(x1), y(y1)
+    {
+    }
+    ~Pt(){}
+    double getX() const {return x;}
+    double getY() const {return y;}
+    void print()
+    {
+        cout << "(" << x << ", " << y << ")";
+    }
+};
+
+class Shape
+{
+public:
+    Shape() {}
+    virtual double area() const = 0;
+    virtual bool inside(const Pt &) const = 0;
+    virtual double perimeter() const = 0;
+    virtual bool degenerate() const = 0;
+    virtual void print() const = 0;
+};
+////////////////////////////////
+
+class Triangle : public Shape
+{
+private:
+    Pt a;
+    Pt b;
+    Pt c;
+public:
+    Triangle(const Pt & = 0.0, const Pt & = 0.0, const Pt & = 0.0);
+    ~Triangle(){};
+    double area() const;
+    bool inside(const Pt& ) const;
+    double perimeter() const;
+    bool degenerate() const;
+    void print() const;
+};
+
+class Rectangle : public Shape
+{
+private:
+    Pt a;
+    Pt b;
+    double length;
+    double width;
+public:
+    Rectangle(const Pt & = 0.0, const Pt & = 0.0);
+    ~Rectangle() {};
+    double area() const;
+    bool inside( const Pt& ) const;
+    double perimeter() const;
+    bool degenerate() const;
+    void print() const;
+};
+
+class Circle : public Shape
+{
+private:
+    Pt center;
+    double radius;
+    const long double piii = 3.14159265358979323846;
+public:
+    Circle(const Pt &, const double &);
+    ~Circle(){};
+    double area() const;
+    bool inside( const Pt& ) const;
+    double perimeter() const;
+    bool degenerate() const;
+    void print() const;
+    double getPi() const;
+
+};
+
+#endif // SHAPE_H
---
+++ Week 10 Polymorphism for Shapes Part 1/main.cpp
@@ -0,0 +1,64 @@
+#include "Shape.h"
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    Pt p1(1, 2);
+    Pt p2(5, 8);
+    Pt p3(-2, 10);
+    Pt p4(0, 0);
+    Pt p5(1.5, 4);
+    Pt p6(2, 8);
+    Pt p7(10, 8);
+    Triangle tr1(p1, p2, p3);
+    Triangle tr2(p2, p3, p4);
+    Triangle tr3(p2, p6, p7);
+    Rectangle rect1(p1, p3);
+    Rectangle rect2(p1, p1);
+    Circle cir1(p4, 10.0);
+    Circle cir2(p2, 4);
+    Rectangle rect3(p1, p2);
+    const int numShape = 8;
+    vector<Shape*> baseShape(numShape);
+    baseShape[0] = &tr1;
+    baseShape[1] = &tr2;
+    baseShape[2] = &rect1;
+    baseShape[3] = &cir1;
+    baseShape[4] = &tr3;
+    baseShape[5] = &rect2;
+    baseShape[6] = &cir2;
+    baseShape[7] = &rect3;
+
+    for (int i = 0; i < numShape; ++i)
+    {
+        baseShape[i]->print();
+        p5.print();
+        if (baseShape[i]->inside(p5))
+        {
+            cout << " is inside this shape." << endl;
+        }
+        else
+        {
+            cout << " is outside this shape." << endl;
+        }
+        Circle *cPtr = dynamic_cast<Circle *>(baseShape[i]);
+        if (cPtr != 0)
+        {
+            cout << "## This shape is a circle." << endl;
+        }
+        Triangle *tPtr = dynamic_cast<Triangle *>(baseShape[i]);
+        if (tPtr != 0)
+        {
+            cout << "## This shape is a triangle." << endl;
+        }
+        Rectangle *rPtr = dynamic_cast<Rectangle *>(baseShape[i]);
+        if (rPtr != 0)
+        {
+            cout << "## This shape is a rectangle." << endl;
+        }
+    }
+    return 0;
+}
